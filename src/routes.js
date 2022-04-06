f7baa8f6079ca2cc1bdc7f6930fe503a9eb0f228
Kyuraz <50727469+anugerahc@users.noreply.github.com>
2022-04-06 19:21:04 +0700

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+const {
+  saveBookHandler,
+  getAllBooksHandler,
+  getAllBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: saveBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getAllBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
